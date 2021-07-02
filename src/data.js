aa52ed3eac0a3778b610856e99d9a3d2c1ca56d3
periwalkeshav <keshav.periwal2018@vitstudent.ac.in>
2021-07-02 17:14:35 +0530

    slider

---
+++ src/data.js
@@ -0,0 +1,76 @@
+const people = [
+  {
+    id: 1,
+    image:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    name: 'maria ferguson',
+    title: 'The BOSS',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam consequuntur iusto ut quia porro eius! Minima, doloremque!',
+  },
+  {
+    id: 2,
+    image:
+      'https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzF8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    name: 'john doe',
+    title: 'Manager',
+    quote:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut inventore doloremque rerum mollitia nobis laudantium, quos aliquam obcaecati culpa sunt?',
+  },
+  {
+    id: 3,
+    image:
+      'https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    name: 'peter smith',
+    title: 'product designer',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nulla ad, officia quo eveniet optio quos assumenda culpa, provident praesentium nostrum vitae atque reprehenderit incidunt.',
+  },
+  {
+    id: 4,
+    image:
+      'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    name: 'susan andersen',
+    title: 'Junior Product Designer',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti magnam maxime excepturi hic, laboriosam eum.',
+  },
+  {
+    id: 5,
+    image:
+      'https://images.unsplash.com/photo-1520466809213-7b9a56adcd45?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nzh8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    name: 'Natalie',
+    title: 'App Developer',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo necessitatibus excepturi adipisci, repellendus ab error sit distinctio dicta reprehenderit at, eaque tenetur.',
+  },
+  {
+    id: 6,
+    image:
+      'https://images.unsplash.com/photo-1464863979621-258859e62245?ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODd8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    name: 'Madeline',
+    title: 'Web Developer',
+    quote:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores explicabo, autem cumque quod perferendis corrupti.',
+  },
+  {
+    id: 7,
+    image:
+      'https://images.unsplash.com/photo-1503185912284-5271ff81b9a8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODZ8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    name: 'Allison',
+    title: 'Intern',
+    quote:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam, fuga.',
+  },
+  {
+    id: 8,
+    image:
+      'https://images.unsplash.com/photo-1531384441138-2736e62e0919?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTN8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    name: 'Samuel',
+    title: 'Photographer',
+    quote:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem aut explicabo vitae? Facere ratione labore officia ipsa! Tenetur cumque nesciunt accusantium eligendi quaerat ipsa impedit, eaque neque incidunt reiciendis maxime?',
+  },
+]
+
+export default people
